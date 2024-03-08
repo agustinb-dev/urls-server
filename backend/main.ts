92726e24e34fe610591be6934b81afb8b78a3284
Agustin <lyricquinn27@gmail.com>
2024-03-07 23:33:05 -0300

    CORS enabled

--- backend/main.ts
+++ backend/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
   const config = new DocumentBuilder()
     .setTitle('url shortener server')
     .setDescription('url shortener API')
